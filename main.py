a87048a69fd79d6f8ec208720daece6c0dfe951b
Tejas Girhe <77229114+TejasGirhe@users.noreply.github.com>
2023-05-01 00:44:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+import os
+from fastapi.responses import FileResponse
+from PIL import ImageTk
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+import pyrebase
+firebaseConfig = {
+  "apiKey": "AIzaSyD4OKbsEvaBQF5QCo-A9TikXE9-8XIquPE",
+  "authDomain": "text-to-image-c4dd6.firebaseapp.com",
+  "projectId": "text-to-image-c4dd6",
+  "storageBucket": "text-to-image-c4dd6.appspot.com",
+  "messagingSenderId": "1060244681808",
+  "appId": "1:1060244681808:web:487a0706a945ca90f84d55",
+  "measurementId": "G-355QF81HNT",
+  "serviceAccount" : "/Users/tejasgirhe/VSCode/TextToImage/text-to-image.json",
+  "databaseURL" : "https://text-to-image-c4dd6-default-rtdb.firebaseio.com/"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+
+auth_token = "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"
+modelid = "CompVis/stable-diffusion-v1-4"
+device = "cpu"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token) 
+pipe.to(device) 
 
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
+async def generate(prompt_text: str): 
+    with autocast(device): 
+        image = pipe(prompt_text, guidance_scale=8.5)
+
+    img = image.images[0]
+    img.save('/TextToImage/image.png', 'PNG')
+    firebase.storage().child(prompt_text + ".png").put('/TextToImage/image.png')
+    return {"Id" : prompt_text}
